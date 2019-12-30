ceac2c4bbf7ff22c140ae317ea6460f2f08365aa
Ross Whitehead <rossshaggywhitehead@hotmail.com>
2019-12-30 11:48:09 +0000

    Implement mongo client retry logic and add service to docker compose

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const { ApolloServer } = require('apollo-server')
 const { RedisCache } = require('apollo-server-cache-redis')
 const MongoClient = require('mongodb').MongoClient
+const promiseRetry = require('promise-retry')
 require('dotenv').config()
 
 const MongoDBDataSource = require('./datasources/mongodb-datasource')
@@ -17,14 +18,34 @@ const {
   REDIS_HOST
 } = process.env
 
-async function main() {
-  const client = new MongoClient(MONGO_URL)
+function main() {
+  const connectOptions = {
+    reconnectTries: 60,
+    reconnectInterval: 1000,
+    poolSize: 10,
+    bufferMaxEntries: 0
+  }
 
-  try {
-    await client.connect()
-    console.log(`Connected to ${MONGO_URL}`)
+  const promiseRetryOptions = {
+    retries: connectOptions.reconnectTries,
+    factor: 1.5,
+    minTimeout: connectOptions.reconnectInterval,
+    maxTimeout: 5000
+  }
+  
+  const connect = (url) => {
+    return promiseRetry((retry, number) => {
+      console.log(`MongoClient connecting to ${url} - retry number: ${number}`)
+      return MongoClient.connect(url, connectOptions).catch(retry)
+    }, promiseRetryOptions)
+  }
+
+  connect(MONGO_URL).then((client) => {
+    console.log("Mongo client connected.")
+    console.log({client})
 
     const db = client.db(MONGO_DB)
+
     const accountsCollection = db.collection(MONGO_ACCOUNTS)
     const customerAccountsCollection = db.collection(MONGO_CUSTOMER_ACCOUNTS)
 
@@ -41,14 +62,11 @@ async function main() {
       }),
       context
     })
-  
-    server.listen().then(({ MONGO_URL }) => {
-      console.log(`🚀 Server ready at ${MONGO_URL}`) 
+
+    server.listen().then(({url}) => {
+      console.log(`🚀 Server ready at ${url}`) 
     })
-  } catch (err) {
-    console.log(err.stack)
-  }
-  client.close()
+  })
 }
 
 main()
